d487f548f28edd61909671547433e4c9e83b54df
LauraM947 <90054045+LauraM947@users.noreply.github.com>
2022-08-03 12:25:20 +0200

    Update MATLAB_CV_Confocal.m

--- CV-Part2-Matlab/MATLAB_CV_Confocal.m
+++ CV-Part2-Matlab/MATLAB_CV_Confocal.m
@@ -1,7 +1,7 @@
-%% Coefficient of variation for confocal images
-% Code by Chiara VICARIO 2019-02
-% Further annotated & README file by Victoria NEGUEMBOR 2021-06
-% Further annotated & README file by Laura MARTIN 2022-08
+%%% Coefficient of variation for confocal images
+%% Code by Chiara VICARIO 2019-02
+%% Further annotated & README file by Victoria NEGUEMBOR 2021-06
+%% Further annotated & README file by Laura MARTIN 2022-08
 
 clear all
 close all 
@@ -11,22 +11,22 @@ edges = 0:1:255;
 
 %% Parameter to add
 
-% Adjust the number and names of categories, and the folder directory where to save output files.
+%% Adjust the number and names of categories, and the folder directory where to save output files.
 categ = 2; 
 Folder = 'C:\Users\';
 categname = {'Condition1','Condition2'};
 
 for m = 1:categ
    
-    % load .tif files 
+    %% load .tif files 
     I{m,1} = uipickfiles;
     
 end
 
-% % %  ATTENTION !The number of colors should be the same of the number of categ!
-% % Uncheck the following line to have Condition1 in magenta and Condition2 in green. Add colors [] if categ>2.
+%%%  ATTENTION !The number of colors should be the same of the number of categ!
+%% Uncomment next line to have Condition1 in magenta and Condition2 in green. Add colors [] if categ>2.
 % colors = [[0.6314    0.0039    0.7569];  [0.5176    0.8902    0.0039]];
-% % Uncheck the following line to assign random colors to Conditions. (if categ = 5, colors = colormap(jet(5));)
+%% Uncomment next line to assign random colors to Conditions. (if categ = 5, colors = colormap(jet(5));)
 colors = colormap(jet(2));
 
 for m = 1:categ
@@ -57,21 +57,21 @@ for k = 1: length(data)
 end
 end
 
-% % % Change plot appearance by modifying axes labels, fontsize etc.
+%%% Change plot appearance by modifying axes labels, fontsize etc.
 xlabel('pixel intensity','fontsize',14)
 ylabel('probability','fontsize',14)
 set(gca,'FontSize',14)
 set(gcf,'color','w');
 grid on 
-%Save figure of individual cells
+%% Save figure of individual cells
 saveas(gcf,strcat(Folder,'\CV_Histplot_individualcells'),'fig')
 
-%Uncomment next line if you also want to save a .bmp of the figure. 
+%% Uncomment next line if you also want to save a .bmp of the figure. 
 %saveas(gcf,strcat(Folder,'\CV_Histplot_individualcells'),'bmp')
 
 
-% Save output CV analysis result
-% ATT no headers!!! 
+%% Save output CV analysis result
+%% ATT no headers!!! 
 CVtable = array2table(CV');
 CVtable.Properties.VariableNames  = categname;
 writetable(CVtable,strcat(Folder,'\CVanalysis_result.xlsx'));
@@ -85,10 +85,10 @@ writetable(CVtable,strcat(Folder,'\CVanalysis_result.xlsx'));
 
 
 
-% Compute meand and SE of px histogram
+%% Compute meand and SE of px histogram
 N_mean = cellfun(@(x)mean(x,2), N,'UniformOutput',false);
 N_std = cellfun(@(x)std(x,1,2)./sqrt(size(x,2)), N,'UniformOutput',false);
-% Plot shade error bar
+%% Plot shade error bar
 for m =1 :categ
     figure(2)
     hold all
@@ -111,22 +111,22 @@ set(gca,'FontSize',18)
 set(gcf,'color','w');
 grid on 
 box off
-% % % Change xlim value according to the maximum intensity of your images (i.e. for 16-bit images, xlim([0 65535]) )
+%%% Change xlim value according to the maximum intensity of your images (i.e. for 16-bit images, xlim([0 65535]) )
 xlim([0 255])
 
 
-%Save the other figure
+%% Save the other figure
 saveas(gcf,strcat(Folder,'\CV_Histplot_smooth'),'fig')
 
-%Uncomment next line if you also want to save a .bmp of the figure. 
+%% Uncomment next line if you also want to save a .bmp of the figure. 
 %saveas(gcf,strcat(Folder,'\CV_Histplot_smooth'),'bmp')
 
-%Save Workspace
+%% Save Workspace
 save(strcat(Folder,'\CVanalysis_workspace.mat'));
 %saveas(gcf,'Density_Results_Histplot_smooth','fig')
 
 
-%Uncomment this part (CTRL+T or command+T for Mac) if you want to plot the boxplot in MatLab 
+%% Uncomment this part (CTRL+T or command+T for Mac) if you want to plot the boxplot in MatLab 
 boxplotpregunta = input('Do Boxplot (Y/N)>','s');
 
 if boxplotpregunta=='Y';
@@ -165,10 +165,8 @@ for m = 1 :categ
     
 end
 
-%Save figure3 boxplot
+%% Save figure3 boxplot
 saveas(gcf,strcat(Folder,'\Coefficient_Variation_Boxplot'),'fig')
 
 else
 end
-
-
